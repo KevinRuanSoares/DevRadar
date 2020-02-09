eb88eaaf38131349990f6ae160165ae188d73767
SoaresKevin <kevin.soares@unifebe.edu.br>
2020-02-09 20:33:05 -0300

    add dowload apk

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import { MdAndroid } from "react-icons/md";
 import DevForm from "./components/DevForm";
 import DevItem from "./components/DevItem";
 
@@ -34,9 +35,14 @@ function App() {
         <ul>
           {devs.map( dev => (
             <DevItem key={dev._id} dev={dev} />
-          ))}
+            ))}
         </ul>
       </main>
+      <footer>
+        <a href="" target="_blank">
+          <MdAndroid size={54} color="#7d40e7"/> 
+        </a>
+      </footer>
     </div>
   );
 }
